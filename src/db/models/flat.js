9ca3785ea08323000e597d76bde5296b2539179f
Caroline Kudrytska <73542400+Carolinealt@users.noreply.github.com>
2025-02-05 18:40:03 +0200

    mongo schema

---
+++ src/db/models/flat.js
@@ -0,0 +1,33 @@
+import { Schema, model } from 'mongoose';
+
+const flatsSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: String,
+            required: true,
+        },
+        rooms: {
+            type: Number,
+            required: true,
+            enum: [1, 2, 3]
+        },
+        photos:
+        {
+            type: Array,
+            required: true
+        }
+    }, {
+    timestamps: true,
+    versionKey: false
+}
+);
+
+export const FlatsCollection = model('flats', flatsSchema);
