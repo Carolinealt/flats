2662bdcc395963bdc542fe3c86aa6088453ff54a
Caroline Kudrytska <73542400+Carolinealt@users.noreply.github.com>
2025-02-08 00:37:03 +0200

    refact FlatForm

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import FlatsList from './components/FlatsList/FlatsList'
 import { useDispatch } from 'react-redux';
 import { fetchFlats } from './redux/flats/operations';
-import FlatForm from './components/FlatForm/FlatForm';
+import FlatForm from './components/FlatForm/FlatForm'
 
 function App() {
   const dispatch = useDispatch();
