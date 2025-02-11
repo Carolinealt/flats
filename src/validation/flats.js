378c17c67e51f56589f9b23ab4980f4dc89a378c
Caroline Kudrytska <73542400+Carolinealt@users.noreply.github.com>
2025-02-11 16:07:04 +0200

    Update flats.js

--- src/validation/flats.js
+++ src/validation/flats.js
@@ -11,12 +11,12 @@ export const createFlatSchema = Joi.object({
         'string.max': 'Description should have at most {#limit} characters',
         'any.required': 'Description is required'
     }),
-    price: Joi.number().required().messages({
-        'number.base': 'Price must be a number',
+    price: Joi.string().required().messages({
+        'number.base': 'Price must be a string',
         'any.required': 'Price is required'
     }),
-    rooms: Joi.valid(1, 2, 3).required().messages({
-        'number.base': 'Number of rooms must be a number',
+    rooms: Joi.valid("1", "2", "3").required().messages({
+        'number.base': 'Number of rooms must be a string',
         'number.valid': 'Number of rooms must contain 1, 2, 3 values only',
         'any.required': 'Number of rooms is required'
     }),
